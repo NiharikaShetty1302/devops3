19b86ec6fb5836a22c14585be4d5ee5d320e1e42
NiharikaShetty1302 <sniharika_cse230554@mgit.ac.in>
2025-09-04 15:35:35 +0530

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ document.getElementById("ecetForm").addEventListener("submit", function (e) {
 
   const phone = document.getElementById("phone").value;
 
-  if (!/^\d{10}$/.test(phone)) {
+  if (!/^(\+91)?\d{10}$/.test(phone)) {
     alert("Phone number must be 10 digits.");
     return;
   }
